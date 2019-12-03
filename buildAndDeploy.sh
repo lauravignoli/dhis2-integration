da82e3bc1be3662771dc5c7d8ec2bca06a74d1c0
lauvignoli <vignoli.laura2@gmail.com>
2019-12-03 09:40:20 +0200

    Add instructions to install rpmbuild

--- buildAndDeploy.sh
+++ buildAndDeploy.sh
@@ -1,6 +1,7 @@
 # To install mvn: https://tecadmin.net/install-apache-maven-on-centos/
 # To install java jdk: sudo yum install java-1.8.0-openjdk-devel
+# To install rpmbuild: sudo yum install rpmdevtools yum-utils
 
-mvn clean install -DskipTests -P-rpm
+mvn clean install -DskipTests
 sudo cp target/dhis-integration-1.0-SNAPSHOT.jar /opt/dhis-integration/bin/dhis-integration.jar
 sudo service dhis-integration restart
