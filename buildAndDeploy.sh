b91ca4bc6b6cc663e5c7b823fd267d2f8a5c564f
lauvignoli <vignoli.laura2@gmail.com>
2019-12-03 09:23:26 +0200

    Add instructions to install java jdk

--- buildAndDeploy.sh
+++ buildAndDeploy.sh
@@ -1,3 +1,6 @@
+# To install mvn: https://tecadmin.net/install-apache-maven-on-centos/
+# To install java jdk: sudo yum install java-1.8.0-openjdk-devel
+
 mvn clean install -DskipTests -P-rpm
 sudo cp target/dhis-integration-1.0-SNAPSHOT.jar /opt/dhis-integration/bin/dhis-integration.jar
 sudo service dhis-integration restart
